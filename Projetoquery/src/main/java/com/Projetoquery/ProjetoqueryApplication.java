af6c27a9560557d5c420758a1b19c1c944397171
Kaikymorelli <132359674+Kaikymorelli@users.noreply.github.com>
2024-04-25 16:40:59 -0300

    Add files via upload

---
+++ Projetoquery/src/main/java/com/Projetoquery/ProjetoqueryApplication.java
@@ -0,0 +1,13 @@
+package com.Projetoquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoqueryApplication.class, args);
+	}
+
+}
